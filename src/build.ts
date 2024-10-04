4ba7973a99d1113ed8b3ba4cc12f46f37c0f3968
Yogesh <yjangid@ebizneeds.com>
2024-10-04 15:56:06 +0530

    changes

--- src/build.ts
+++ src/build.ts
@@ -5,9 +5,7 @@ import path from 'path';
 import commandLineArgs from 'command-line-args';
 
 
-// Function to build and copy .env file
 export default function buildAndCopyEnv() {
-    // Run tsc to compile TypeScript code
     exec('tsc', (error: any) => {
         if (error) {
             console.error(`Error during build: ${error.message}`);
@@ -15,7 +13,7 @@ export default function buildAndCopyEnv() {
         }
 
 
-        console.log('Build successful!');
+        console.log('Build successfull!');
 
         const options = commandLineArgs([
             {
@@ -26,23 +24,14 @@ export default function buildAndCopyEnv() {
             }
         ])
 
-        // Define the source and destination paths for the .env file
         const sourcePath = path.resolve(process.cwd(),"common","env", `${String(options.env)}.env`);
         const distPath = path.resolve(process.cwd(), 'dist');
 
-
-        console.log('Current working directory:', process.cwd());
         console.log(sourcePath);
-        console.log("distPath>>>>>>>>>>", distPath);
 
-
-        // Check if .env file exists
         if (fs.existsSync(sourcePath)) {
-            // Copy the .env file to the dist folder
-
             fs.copyFileSync(sourcePath, distPath);
             console.log('.env file copied to dist folder!');
-
         } else {
             console.error('.env file does not exist!');
         }
--- src/common/database/db.ts
+++ src/common/database/db.ts
@@ -6,13 +6,6 @@ const DB_URL: string = process.env.DB_URL || '';
 
 mongoose.set('strictQuery', false);
 mongoose.connect(DB_URL, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true
 })
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
-
-
-// mongoose.connection.on("disconnect", () => {
-//     console.log("MongoDB disconnected");
-// });
